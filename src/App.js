da2065bc84811112bfdae0b3da0ed99692dc1770
Ale <aleevelarde9@gmail.com>
2020-01-28 22:16:35 -0600

    App.js actualizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 
-function App() {
-  return (
-    <div className="app">
-      <Header
-        titulo = 'Clima React'
-      />
-      <Formulario/>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className="app">
+        <Header
+          titulo = 'Clima React'
+        />
+        <Formulario/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
