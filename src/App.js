6aa142f3e01447a6449acd4b854a19bbe2281573
Ale <aleevelarde9@gmail.com>
2020-01-28 22:22:18 -0600

    datosConsulta agregado al app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Formulario from './componentes/Formulario';
 
 class App extends Component {
 
+  datosConsulta = respuesta => {
+    console.log(respuesta); 
+  }
+
   render(){
     return (
       <div className="app">
         <Header
           titulo = 'Clima React'
         />
-        <Formulario/>
+        <Formulario
+          datosConsulta = {this.datosConsulta}
+        />
       </div>
     );
   }
