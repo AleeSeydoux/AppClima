424f80127e30f2c499d8abd9a357094e225c0c38
Ale <aleevelarde9@gmail.com>
2020-01-30 16:27:17 -0600

    Clima.js importado en App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 import Error from './componentes/Error';
+import Clima from './componentes/Clima';
 
 class App extends Component {
 
@@ -56,7 +57,8 @@ class App extends Component {
       })
     } else {
       this.setState({
-        consulta: respuesta
+        consulta: respuesta,
+        error : false
       })
     }
   }
@@ -68,6 +70,8 @@ class App extends Component {
 
     if(error){
       resultado = <Error mensaje= "Ambos campos son obligatorios"/>
+    } else {
+      resultado = <Clima/>
     }
 
     return (
