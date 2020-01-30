080088f586dc41ceabff4f72258c9131abcf9c71
Ale <aleevelarde9@gmail.com>
2020-01-30 17:11:42 -0600

    Clima.js actualizado

--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class Clima extends Component {
     render() { 
+        console.log(this.props.resultado);
         return ( 
             <div className="container">
                 <h1>desde clima.js</h1>
