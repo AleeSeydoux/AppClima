17ae6aea10c83ef83d781344213236710d5b262c
Ale <aleevelarde9@gmail.com>
2020-02-06 22:57:48 -0600

    PropTypes añadidos

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = (props) => {
     return (
@@ -12,5 +13,9 @@ const Header = (props) => {
         </div>
     );
 };
+
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
+}
 
 export default Header;
